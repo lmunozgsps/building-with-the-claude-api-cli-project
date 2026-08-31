5c03c7d2897c0489855aafeb45bfacd97ab2b1f9
Leonardo Muñoz <lmunozg@spsolutions.com.mx>
2026-08-30 20:54:43 -0600

    feat(tools): implementa read_doc_contents y edit_document

--- mcp_server.py
+++ mcp_server.py
@@ -1,19 +1,48 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("DocumentMCP", log_level="ERROR")
+mcp = FastMCP("DocumentMCP", log_level = "ERROR")
 
 
 docs = {
-    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
-    "report.pdf": "The report details the state of a 20m condenser tower.",
-    "financials.docx": "These financials outline the project's budget and expenditures.",
-    "outlook.pdf": "This document presents the projected future performance of the system.",
-    "plan.md": "The plan outlines the steps for the project's implementation.",
-    "spec.txt": "These specifications define the technical requirements for the equipment.",
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E."
+    , "report.pdf": "The report details the state of a 20m condenser tower."
+    , "financials.docx": "These financials outline the project's budget and expenditures."
+    , "outlook.pdf": "This document presents the projected future performance of the system."
+    , "plan.md": "The plan outlines the steps for the project's implementation."
+    , "spec.txt": "These specifications define the technical requirements for the equipment."
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+
+@mcp.tool(
+    name = "read_doc_contents"
+    , description = "Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description = "ID of the document to read")
+):
+
+    if doc_id not in docs:
+        raise ValueError(f"Doc with ID {doc_id} not found :(")
+
+    return docs[doc_id]
+
+
+@mcp.tool(
+    name = "edit_document"
+    , description = "Edit a document by replacing a string in the documents content with a new string"
+)
+def edit_document(
+    doc_id: str = Field(description = "ID of the document that will be edited")
+    , old_str: str = Field(description = "The text to replace must match exactly, including whitespace")
+    , new_str: str = Field(description = "The new text to insert in place of the old text")
+):
+
+    if doc_id not in docs:
+        raise ValueError(f"Doc with ID {doc_id} not found :(")
+
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
@@ -21,4 +50,4 @@ docs = {
 
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    mcp.run(transport = "stdio")
